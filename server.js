709680b7fcdbf424eda97ff08e5132b119c65782
ypulido28 <ypulido46@misena.edu.co>
2023-05-12 10:58:33 -0500

    [UPDATE]server.js

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const path = require('path');
 const app = express();
 
 // Configura la carpeta "build" de React para servir como carpeta estática
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'ui/whiteboard-collab/build')));
 
-// Configura una ruta de prueba para el backend
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+
 
 // Configura una ruta para que todas las demás solicitudes se dirijan a la aplicación React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui/whiteboard-collab/src/App.js', 'index.html'));
 });
 
 // Inicia el servidor en el puerto 5000
